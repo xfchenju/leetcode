75b5469c1b106542c49625f891e632bacbdc8e5f
xfchenju <975982556@qq.com>
2021-04-22 17:07:01 +0800

    2021年4月22日17:07:00

---
+++ 链表/Node.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: chenju
+ * @Date: 2021-04-22 14:55:59
+ * @LastEditors: chenju
+ * @LastEditTime: 2021-04-22 14:59:56
+ */
+function Node (val, next, random) {
+  this.val = val;
+  this.next = next;
+  this.random = random
+}
