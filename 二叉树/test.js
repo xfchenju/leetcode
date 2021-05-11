7e6eb5928dcd6fab0ecf282283aae7da9bcdad9d
xfchenju <975982556@qq.com>
2021-05-11 17:57:52 +0800

    2021年5月11日17:57:50

---
+++ 二叉树/test.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Author: chenju
+ * @Date: 2021-05-11 16:31:17
+ * @LastEditors: chenju
+ * @LastEditTime: 2021-05-11 16:32:09
+ */
+let arr = []; 
+if (!arr[1]) arr.push([])
+console.log(arr)
